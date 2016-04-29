b1ff1c898be9edfee962dea92e0bb583500309c3
Guido Contreras Woda <guiwoda@gmail.com>
2016-04-29 00:53:39 -0300

    Progreso con ventanilla

--- src/Ventanilla.java
+++ src/Ventanilla.java
@@ -1,4 +1,7 @@
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class Ventanilla {
 	private Map<Integer, CuentaDeCliente<?>> cuentas;
@@ -8,14 +11,56 @@ public class Ventanilla {
 	}
 	
 	public CuentaDeCliente<?> depositar(int CBU, Dinero monto) throws Exception {
+		CuentaDeCliente<?> cuenta = get(CBU);
+		
+		cuenta.depositar(monto);
+		
+		return cuenta;
+	}
+
+	public CajaDeAhorro extraer(int CBU, Dinero monto) throws Exception {
+		CuentaDeCliente<?> cuenta = get(CBU);
+		
+		if (! (cuenta instanceof CajaDeAhorro)) {
+			throw new Exception("Solo se puede extraer de una caja de ahorro.");
+		}
+		
+		cuenta.extraer(monto);
+		
+		return (CajaDeAhorro) cuenta;
+	}
+	
+	public void transferir(int origen, int destino, Dinero monto) throws Exception {
+		CuentaDeCliente<?> cuentaOrigen = get(origen);
+		CuentaDeCliente<?> cuentaDestino = get(destino);
+		
+		cuentaOrigen.transferir(cuentaDestino, monto);
+	}
+	
+	private CuentaDeCliente<?> get(int CBU) throws Exception {
 		CuentaDeCliente<?> cuenta = cuentas.get(CBU);
 		
 		if (cuenta == null) {
 			throw new Exception("No se encontró la cuenta con el CBU: " + String.valueOf(CBU));
 		}
 		
-		cuenta.depositar(monto);
-		
 		return cuenta;
+	}
+
+	public Set<Transaccion> movimientos(int cbu) throws Exception {
+		return get(cbu).getTransacciones();
+	}
+
+	public Set<Transaccion> movimientos(int cbu, int cantidad) throws Exception {
+		Set<Transaccion> result = new HashSet<>();
+		Iterator<Transaccion> it = get(cbu).getTransacciones().iterator();
+		
+		int i = 0;
+		while (i < cantidad && it.hasNext()) {
+			result.add(it.next());
+			i++;
+		}
+		
+		return result;
 	}
 }
--- tests/VentanillaTest.java
+++ tests/VentanillaTest.java
@@ -1,7 +1,11 @@
 import static org.junit.Assert.*;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,10 +21,13 @@ public class VentanillaTest extends TrabajoPracticoTest<Ventanilla> {
 			
 			cuentas.put(cuenta.getCBU(), cuenta);
 		}
+		
+		cuentas.put(1, CuentasFixture.cajaDeAhorroPesos());
+		cuentas.put(2, CuentasFixture.cuentaCorriente());
 	}
 	
 	@Override
-	public Ventanilla getObject() {
+	public Ventanilla getObject() throws Exception {
 		return new Ventanilla(cuentas);
 	}
 
@@ -64,62 +71,147 @@ public class VentanillaTest extends TrabajoPracticoTest<Ventanilla> {
 	}
 	
 	@Test
-	public void puedeExtraerEfectivoDeUnaCajaDeAhorroHabilitada() {
+	public void puedeExtraerEfectivoDeUnaCajaDeAhorroHabilitada() throws Exception {
+		int CBU = 1;
+		CuentaDeCliente<?> cuenta = cuentas.get(CBU);
+		Dinero saldo = cuenta.getSaldo();
+
+		CajaDeAhorro result = getObject().extraer(CBU, new Dinero(cuenta.getMoneda(), 10));
+		
+		assertEquals(cuenta, result);
+		assertTrue(result.getSaldo().compareTo(saldo) < 0);
+	}
+	
+	@Test(expected=Exception.class)
+	public void noPuedeExtraerEfectivoDeUnaCajaDeAhorroInactiva() throws Exception {
+		int CBU = 1;
+		CuentaDeCliente<?> cuenta = cuentas.get(CBU);
+
+		cuenta.inactivar();
+		
+		getObject().depositar(CBU, new Dinero(cuenta.getMoneda(), 1000));
+	}
+	
+	@Test(expected=Exception.class)
+	public void noPuedeExtraerMasDelSaldoDisponibleDeUnaCajaDeAhorro() throws Exception {
+		int CBU = 1;
+		CuentaDeCliente<?> cuenta = cuentas.get(CBU);
+		Dinero saldo = cuenta.getSaldo();
+
+		getObject().extraer(CBU, saldo.sumar(10));
+	}
+	
+	@Test
+	public void puedeTransferirDineroEntreDosCuentasHabilitadas() throws Exception {
+		Iterator<Integer> it = cuentas.keySet().iterator();
+		
+		int origen = it.next();
+		int destino = it.next();
+		
+		CuentaDeCliente<?> cuentaOrigen = cuentas.get(origen);
+		CuentaDeCliente<?> cuentaDestino = cuentas.get(destino);
+		
+		Dinero saldoOrigen = cuentaOrigen.getSaldo();
+		Dinero saldoDestino = cuentaDestino.getSaldo();
+		
+		getObject().transferir(origen, destino, cuentaOrigen.getSaldo());
+		
+		assertTrue(cuentaOrigen.getSaldo().compareTo(saldoOrigen) < 0);
+		assertTrue(cuentaDestino.getSaldo().compareTo(saldoDestino) > 0);
+	}
+	
+	@Test(expected=Exception.class)
+	public void noPuedeTransferirDineroDesdeCuentasInactivas() throws Exception {
+		Iterator<Integer> it = cuentas.keySet().iterator();
+		
+		int origen = it.next();
+		int destino = it.next();
+		
+		CuentaDeCliente<?> cuentaOrigen = cuentas.get(origen);
+		cuentaOrigen.inactivar();
+		
+		getObject().transferir(origen, destino, new Dinero(Moneda.PESO, 5));
+	}
+	
+	@Test(expected=Exception.class)
+	public void noPuedeTransferirDineroHaciaCuentasInactivas() throws Exception {
+		Iterator<Integer> it = cuentas.keySet().iterator();
+		
+		int origen = it.next();
+		int destino = it.next();
+		
+		CuentaDeCliente<?> cuentaDestino = cuentas.get(destino);
+		cuentaDestino.inactivar();
+		
+		getObject().transferir(origen, destino, new Dinero(Moneda.PESO, 5));
+	}
+	
+	@Test(expected=Exception.class)
+	public void noPuedeTransferirMasDineroDelSaldoQueTengaLaCuentaDeOrigen() throws Exception {
+		Iterator<Integer> it = cuentas.keySet().iterator();
+		
+		int origen = it.next();
+		int destino = it.next();
+		
+		CuentaDeCliente<?> cuentaOrigen = cuentas.get(origen);
+		
+		getObject().transferir(origen, destino, cuentaOrigen.getSaldo().sumar(1));
+	}
+	
+	@Test(expected=Exception.class)
+	public void noPuedeTransferirDineroSiElSaldoNoAlcanzaAdemasParaCobrarLosImpuestosCorrespondientes() throws Exception {
+		Iterator<Integer> it = cuentas.keySet().iterator();
+		
+		int origen = 2;
+		int destino = it.next();
+		
+		CuentaCorriente cuentaOrigen = (CuentaCorriente) cuentas.get(origen);
+		
+		getObject().transferir(origen, destino, cuentaOrigen.getSaldo().sumar(cuentaOrigen.getSobregiro()));
+	}
+	
+	@Test
+	public void debeConvertirLaMonedaAlTipoDeDestinoUsandoElCambioVigente() throws Exception {
 		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void noPuedeExtraerEfectivoDeUnaCajaDeAhorroInactiva() {
+	public void debeRegistrarLaMonedaElMontoYElTipoDeCambioEnElCampoDeObservacionesSiHuboUnaConversion() throws Exception {
 		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void noPuedeExtraerMasDelSaldoDisponibleDeUnaCajaDeAhorro() {
-		fail("Not yet implemented");
+	public void puedeListarTodosLosMovimientosDeUnaCuenta() throws Exception {
+		CuentaDeCliente<?> cuenta = cuentas.get(cuentas.keySet().iterator().next());
+		
+		assertEquals(
+			cuenta.getTransacciones(), 
+			getObject().movimientos(cuenta.getCBU())
+		);
 	}
 	
 	@Test
-	public void puedeTransferirDineroEntreDosCuentasHabilitadas() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void noPuedeTransferirDineroDesdeCuentasInactivas() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void noPuedeTransferirDineroHaciaCuentasInactivas() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void noPuedeTransferirMasDineroDelSaldoQueTengaLaCuentaDeOrigen() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void noPuedeTransferirDineroSiElSaldoNoAlcanzaAdemasParaCobrarLosImpuestosCorrespondientes() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void debeConvertirLaMonedaAlTipoDeDestinoUsandoElCambioVigente() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void debeRegistrarLaMonedaElMontoYElTipoDeCambioEnElCampoDeObservacionesSiHuboUnaConversion() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void puedeListarTodosLosMovimientosDeUnaCuenta() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void puedeListarLosUltimosNMovimientosDeUnaCuenta() {
-		fail("Not yet implemented");
+	public void puedeListarLosUltimosNMovimientosDeUnaCuenta() throws Exception {
+		CuentaDeCliente<?> cuenta = cuentas.get(cuentas.keySet().iterator().next());
+		cuenta.depositar(new Dinero(cuenta.getMoneda(), 3));
+		cuenta.depositar(new Dinero(cuenta.getMoneda(), 3));
+		cuenta.depositar(new Dinero(cuenta.getMoneda(), 3));
+		cuenta.depositar(new Dinero(cuenta.getMoneda(), 3));
+		
+		Set<Transaccion> expected = new HashSet<>();
+		
+		int i = 0;
+		
+		Iterator<Transaccion> it = cuenta.getTransacciones().iterator();
+		
+		while (i < 3 && it.hasNext()) {
+			expected.add(it.next());
+			i++;
+		}
+			
+		assertEquals(
+			expected,
+			getObject().movimientos(cuenta.getCBU(), 3)
+		);
 	}
 }
