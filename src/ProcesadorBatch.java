949195e3ba2a7e544327a04ce40103346a166ee9
Teo Upenieks Bracco <teoupk@yahoo.com.ar>
2016-05-01 20:58:49 -0300

    Algo chueco :/

--- src/ProcesadorBatch.java
+++ src/ProcesadorBatch.java
@@ -1,4 +1,24 @@
-
+import java.io.*;
 public class ProcesadorBatch {
+	
+	public void cobroDeMantenimientos(){
+		
+	
+		
+	}
 
+	public static void main (String [] args) throws Exception{
+		
+		CuentaEspecial mantenimientos = Banco.instance().getMantenimientos();
+		
+		PrintWriter writer1 = new PrintWriter("MantenimientosCobrados.txt", "UTF-8");
+		PrintWriter writer2 = new PrintWriter("ErroresManteniemiento.txt", "UTF-8");
+		writer1.println("");
+		writer1.println("");
+		writer2.println("");
+		writer2.println("");
+		writer1.close();
+		writer2.close();
+		
+    }
 }
