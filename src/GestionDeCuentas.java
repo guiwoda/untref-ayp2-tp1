11018bde57e8d89f3a0146e9b6ee55ff92eeddbb
Teo Upenieks Bracco <teoupk@yahoo.com.ar>
2016-05-01 21:56:00 -0300

    Para que vaya quedando

--- src/GestionDeCuentas.java
+++ src/GestionDeCuentas.java
@@ -1,5 +1,31 @@
-import java.util.HashSet;
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class GestionDeCuentas {
+	Map<String, Cliente> clientes = new HashMap<>();
+	Map<Documento, PersonaFisica> personasFisicas = new HashMap<>();
+	Map<String, PersonaJuridica> personasJuridicas = new HashMap<>();
+    
+	public double depositoInicial;
+	
+public void aperturaDeCajaDeAhorro(PersonaFisica cliente, double depositoInicial, double costoDeMantenimiento)throws Exception { 
+	this.depositoInicial = depositoInicial;
+	
+}
+
+public void aperturaDeCuentaCorriente( double depositoInicial)throws Exception {
+	
+	
+}
+
+public void inhabilitarCuenta() {
+	
+	
+}
+
+public void habilitarCuenta() {
+	
+	
+}
+
 }
