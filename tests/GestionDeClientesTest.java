55ad2ad6f0460a20e74fcbc90aa2cfa3e2b45257
juanr <juanr@192.168.1.11>
2016-05-02 22:15:35 -0300

    Ctrl+Shift+F > 9 años de lengua en la escuela

--- tests/GestionDeClientesTest.java
+++ tests/GestionDeClientesTest.java
@@ -3,71 +3,81 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class GestionDeClientesTest extends TrabajoPracticoTest<GestionDeClientes> {
+public class GestionDeClientesTest {
 	Cliente cliente;
 	Domicilio casa;
 	PersonaFisica persona;
 	PersonaJuridica empresa;
+	GestionDeClientes gestion;
+
 	@Before
-	
-	public void setUp() throws Exception{
+	public void setUp() throws Exception {
+		gestion = getObject();
 		casa = new Domicilio("La julia 338", 8877, "Saenz Peña", "Buenos Aires");
-		cliente = getObject().darDeAltaPersonaFisica("Ivan", "8-97928549-40", casa, "4840-8843", Documento.dni(49398370), EstadoCivil.SOLTERO, "Desempleado", "Familia");
-		persona = getObject().darDeAltaPersonaFisica("Ivan", "8-97923459-40", casa, "4840-8843", Documento.dni(49398370), EstadoCivil.SOLTERO, "Desempleado", "Familia");
-		empresa = getObject().darDeAltaPersonaJuridica("Patagonica", "20-3194850-90", casa, "4848-8839" );
+		cliente = gestion.darDeAltaPersonaFisica("Ivan", "8-97928549-40", casa,
+				"4840-8843", Documento.dni(49398370), EstadoCivil.SOLTERO,
+				"Desempleado", "Familia");
+		persona = gestion.darDeAltaPersonaFisica("Ivan", "8-97923459-40", casa,
+				"4840-8843", Documento.dni(49398370), EstadoCivil.SOLTERO,
+				"Desempleado", "Familia");
+		empresa = gestion.darDeAltaPersonaJuridica("Patagonica",
+				"20-3194850-90", casa, "4848-8839");
 	}
-	@Override
+
 	protected GestionDeClientes getObject() {
 		return new GestionDeClientes();
 	}
-	
+
 	@Test
-	public void puedeDarDeAltaClientesNuevos() throws Exception{
+	public void puedeDarDeAltaClientesNuevos() throws Exception {
 		assertEquals(true, cliente.isActivo());
 	}
-	
+
 	@Test(expected = Exception.class)
-	public void noPuedeDarDeAltaNuevamenteClientesYaExistentes() throws Exception{
-		
-		getObject().darDeAltaPersonaFisica("Ivan", "8-97928549-40", casa, "4840-8843", Documento.dni(49398370), EstadoCivil.SOLTERO, "Desempleado", "Familia");
+	public void noPuedeDarDeAltaNuevamenteClientesYaExistentes()
+			throws Exception {
+		gestion.darDeAltaPersonaFisica("Ivan", "8-97928549-40", casa,
+				"4840-8843", Documento.dni(49398370), EstadoCivil.SOLTERO,
+				"Desempleado", "Familia");
 	}
-	
+
 	@Test
-	public void puedeDarDeBajaClientesSinCuentasAsociadas() throws Exception{
-		getObject().darDeBaja("8-97928549-40");
+	public void puedeDarDeBajaClientesSinCuentasAsociadas() throws Exception {
+		gestion.darDeBaja("8-97928549-40");
 		assertEquals(false, cliente.isActivo());
 	}
-	
+
 	@Test(expected = Exception.class)
 	public void noPuedeDarDeBajaClientesConCuentasAsociadas() throws Exception {
-		CuentasFixture.cajaDeAhorroPesos ((PersonaFisica) cliente);
-		getObject().darDeBaja("8-97928549-40");
+		CuentasFixture.cajaDeAhorroPesos((PersonaFisica) cliente);
+		gestion.darDeBaja("8-97928549-40");
 	}
-	
+
 	@Test
-	public void puedeBuscarClientesPorCUIT() {
+	public void puedeBuscarClientesPorCUIT() throws Exception {
 		Cliente otro;
-		otro = getObject().buscarClientesPorCuit("8-97928549-40");
+		otro = gestion.buscarClientesPorCuit("8-97928549-40");
 		assertEquals(cliente, otro);
 	}
+
 	@Test
-	public void puedeBuscarClientesPorRazonSocial() {
+	public void puedeBuscarClientesPorRazonSocial() throws Exception {
 		PersonaJuridica otro;
-		otro = getObject().buscarClientesPorRazonSocial("Patagonica");
+		otro = gestion.buscarClientesPorRazonSocial("Patagonica");
 		assertEquals(empresa, otro);
 	}
-	
+
 	@Test
-	public void puedeBuscarClientesPorNumeroDeDocumento() throws Exception{
+	public void puedeBuscarClientesPorNumeroDeDocumento() throws Exception {
 		PersonaFisica otro;
-		otro = getObject().buscarClientesPorNumeroDeDocumento(Documento.dni(49398370));
+		Documento doc = Documento.dni(49398370);
+		otro = gestion.buscarClientesPorNumeroDeDocumento(doc);
 		assertEquals(persona, otro);
-		
-	}	
-	
-	
+
+	}
+
 	@Test
-	public void puedeReactivarUnClienteQueEsteDadoDeBaja() {
+	public void puedeReactivarUnClienteQueEsteDadoDeBaja() throws Exception {
 		cliente.desactivar();
 		cliente.reactivar();
 		assertEquals(true, cliente.isActivo());
